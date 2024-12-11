8ce409a8d06d5356b31e817e61eeea688212d0ad
Huanz86251 <3113405902@qq.com>
2024-12-11 13:43:47 -0500

    RISE.py

--- RISE.py
+++ RISE.py
@@ -25,11 +25,11 @@ test_dataset = PathMNIST(split='test', transform=transform, download=True,size=2
 class RISE:
     def __init__(self, model, input_size, num_masks=5000, mask_size=8):
         """
-        初始化 RISE 解释器
-        :param model: 待解释的模型
-        :param input_size: 输入图像的大小 (H, W)
-        :param num_masks: 随机遮罩的数量
-        :param mask_size: 遮罩的低分辨率大小
+        
+        :param model: 
+        :param input_size: 
+        :param num_masks: 
+        :param mask_size: 
         """
         self.model = model
         self.input_size = input_size
@@ -38,9 +38,7 @@ class RISE:
         self.masks = self._generate_masks()
 
     def _generate_masks(self):
-        """
-        生成随机遮罩，尺寸为 (num_masks, H, W)
-        """
+
         masks = np.random.choice([0, 1], size=(self.num_masks, self.mask_size, self.mask_size), p=[0.5, 0.5])
         masks = masks.astype(np.float32)
         masks = np.array([
@@ -49,12 +47,6 @@ class RISE:
         return torch.tensor(masks, dtype=torch.float32).unsqueeze(1)  # (num_masks, 1, H, W)
 
     def explain(self, image, target_class):
-        """
-        生成 RISE 热图
-        :param image: 输入图像 (C, H, W)
-        :param target_class: 目标类别
-        :return: 热图 (H, W)
-        """
         image = image.unsqueeze(0)
         batch_size = 32
         heatmap = torch.zeros(self.input_size, dtype=torch.float32)
